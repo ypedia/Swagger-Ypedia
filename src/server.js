60b5543a38c53a8fda9b620d56e2990f118a4dc2
Henrique Leao <henrique.s3.leao@gmail.com>
2021-05-06 12:00:32 -0300

    feat: add heruko config file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 
-const { router } = require('./routes');
 const swaggerFile = require('./swagger.json');
 
 const PORT = process.env.PORT || 3333;
@@ -10,8 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
